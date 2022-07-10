800051c58e853b20c9a783698be592fd4d074f81
zl0i <dmitri.popov.96@mail.ru>
2022-07-10 17:18:36 +0300

    style: fixtures.ts

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -1,52 +1,52 @@
 export default {
     alerts: {
-        'receiver': 'receiver1',
-        'status': 'firing',
-        'alerts': [{
-            'status': 'firing',
-            'labels': {
-                'alertname': 'DiskSpace',
-                'device': '/dev/sda1',
-                'fstype': 'ext4',
-                'host': 'foo',
-                'instance': '1.2.3.4',
-                'job': 'node',
-                'mountpoint': '/'
+        "receiver": "receiver1",
+        "status": "firing",
+        "alerts": [{
+            "status": "firing",
+            "labels": {
+                "alertname": "DiskSpace",
+                "device": "/dev/sda1",
+                "fstype": "ext4",
+                "host": "foo",
+                "instance": "1.2.3.4",
+                "job": "node",
+                "mountpoint": "/"
             },
-            'annotations': {
-                'description': 'Filesystem low',
-                'summary': 'Instance 1.2.3.4 filesystem usage is dangerously high'
+            "annotations": {
+                "description": "Filesystem low",
+                "summary": "Instance 1.2.3.4 filesystem usage is dangerously high"
             },
-            'startsAt': '2018-12-05T23:38:43.700314857Z',
-            'endsAt': '0001-01-01T00:00:00Z',
-            'generatorURL': 'http://1.2.3.4/graph?1'
+            "startsAt": "2018-12-05T23:38:43.700314857Z",
+            "endsAt": "0001-01-01T00:00:00Z",
+            "generatorURL": "http://1.2.3.4/graph?1"
         }, {
-            'status': 'firing',
-            'labels': {
-                'alertname': 'DiskSpace',
-                'device': '/dev/sda1',
-                'fstype': 'ext4',
-                'host': 'foo',
-                'instance': '1.2.3.4',
-                'job': 'node',
-                'mountpoint': '/'
+            "status": "firing",
+            "labels": {
+                "alertname": "DiskSpace",
+                "device": "/dev/sda1",
+                "fstype": "ext4",
+                "host": "foo",
+                "instance": "1.2.3.4",
+                "job": "node",
+                "mountpoint": "/"
             },
-            'annotations': {
-                'description': 'Filesystem low',
-                'summary': 'Instance 1.2.3.4 filesystem usage is dangerously high'
+            "annotations": {
+                "description": "Filesystem low",
+                "summary": "Instance 1.2.3.4 filesystem usage is dangerously high"
             },
-            'startsAt': '2018-12-05T23:38:43.700314857Z',
-            'endsAt': '0001-01-01T00:00:00Z',
-            'generatorURL': 'http://1.2.3.4/graph?1'
+            "startsAt": "2018-12-05T23:38:43.700314857Z",
+            "endsAt": "0001-01-01T00:00:00Z",
+            "generatorURL": "http://1.2.3.4/graph?1"
         }],
-        'groupLabels': {},
-        'commonLabels': {
-            'job': 'node'
+        "groupLabels": {},
+        "commonLabels": {
+            "job": "node"
         },
-        'commonAnnotations': {},
-        'externalURL': 'http://1.2.3.4',
-        'version': '4',
-        'groupKey': '{}:{}'
+        "commonAnnotations": {},
+        "externalURL": "http://1.2.3.4",
+        "version": "4",
+        "groupKey": "{}:{}"
     }
 }
 
